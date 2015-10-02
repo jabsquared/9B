3cca7db3c410971fd97d00f04505660623c21cd7
Yonas Berhe <sanoy@Yonass-MacBook-Air.local>
2015-10-01 20:31:45 -0700

    added node serve file

---
+++ index.js
@@ -0,0 +1,22 @@
+var path = require('path');
+var fs = require('fs');
+var server = require('http').createServer(handler);
+
+function handler(req, res) {
+    var url = (req.url.split('?')[0]).substr(1);
+    if (url == '') url = 'index.html';
+
+ fs.readFile( 
+   path.join (process.cwd(), url),
+	function (err, data) {
+          if (err) {
+             res.writeHead(404);
+             return res.end('Err Loading ' + url);
+     }
+     res.writeHead(200);
+     res.end(data);
+ } 
+ ); 
+}
+server.listen(process.env.PORT || 4812);
+console.log('go to localhost:4812');
